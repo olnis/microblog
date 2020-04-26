646e1309947a4a3a23f33b80db82734039d1417c
Tk <atsutaka@gmail.com>
2020-04-26 20:13:04 +0900

    heroku.py fixed

--- microblog/settings/heroku.py
+++ microblog/settings/heroku.py
@@ -9,10 +9,22 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {'default': dj_database_url.config()}
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
