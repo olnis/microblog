8c2ad9956fdf6ae20fcbad50210aa34b92a16acb
Tk <atsutaka@gmail.com>
2020-04-26 16:52:09 +0900

    Heroku ready

--- microblog/settings/heroku.py
+++ microblog/settings/heroku.py
@@ -1,19 +1,18 @@
 from .common import *
 
+import dj_database_url
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] = dj_database_url.config()
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
