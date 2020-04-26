a927f93f80eca8aaa5adea106015dacaff4c3e1b
Tk <atsutaka@gmail.com>
2020-04-26 17:44:38 +0900

    heroku.py fixed

--- microblog/settings/heroku.py
+++ microblog/settings/heroku.py
@@ -13,6 +13,6 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {'default': dj_database_url.config()}
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
